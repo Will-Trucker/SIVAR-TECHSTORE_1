c27564953456436c86a29b2b900e0a699057acf6
William Emmanuel Mazariego Orellana <83888568+Will-Trucker@users.noreply.github.com>
2022-08-23 19:41:25 -0600

    Estetica en Detalles

--- SIVAR-CODE/laravel-mvc/resources/views/detalle.blade.php
+++ SIVAR-CODE/laravel-mvc/resources/views/detalle.blade.php
@@ -46,7 +46,7 @@
                 <a  class="cta" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                       {{ __('Salir') }}   
+                                       {{ __('SALIR') }}   
                                     </a>
         </div>
     </div>
@@ -65,7 +65,7 @@
       <div class="PrecioD">
         <h2 class="PrecioNumero">$82.99</h2>
       </div>
-        <a href="#" class="agregar-carrito" data-id="1"><p>Agregar Al Carrito</p></a>
+        <a href="#" class="agregar-carrito" data-id="1"><p><svg class="carretilla"xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentcolor" d="M96 0C107.5 0 117.4 8.19 119.6 19.51L121.1 32H541.8C562.1 32 578.3 52.25 572.6 72.66L518.6 264.7C514.7 278.5 502.1 288 487.8 288H170.7L179.9 336H488C501.3 336 512 346.7 512 360C512 373.3 501.3 384 488 384H159.1C148.5 384 138.6 375.8 136.4 364.5L76.14 48H24C10.75 48 0 37.25 0 24C0 10.75 10.75 0 24 0H96zM128 464C128 437.5 149.5 416 176 416C202.5 416 224 437.5 224 464C224 490.5 202.5 512 176 512C149.5 512 128 490.5 128 464zM512 464C512 490.5 490.5 512 464 512C437.5 512 416 490.5 416 464C416 437.5 437.5 416 464 416C490.5 416 512 437.5 512 464z"/></svg> Agregar Al Carrito</p></a>
       </div>
 
         <div class="DivInformacion">
@@ -87,13 +87,13 @@
 
 <div class="container">
   <div class="img_container">
-      <img src="{{asset('img/descarga.jpg')}}" alt="" class="main_img">
+      <img src="{{asset('img/mouse.png')}}" alt="" class="main_img">
   </div>
 
   <div class="thumbnail_container">
-      <img src="{{asset('img/descarga.jpg')}}" alt="" class="thumbnail active">
-      <img src="{{asset('img/ml240l-v2-rgb-white-380x380-1-hover.png')}}" alt="" class="thumbnail">
-      <img src="{{asset('img/images.jpg')}}" alt="" class="thumbnail">
+      <img src="{{asset('img/mouse.png')}}" alt="" class="thumbnail active">
+      <img src="{{asset('img/mouse.png')}}" alt="" class="thumbnail">
+      <img src="{{asset('img/mouse.png')}}" alt="" class="thumbnail">
 
   </div>
   <script src="{{asset('js/main.js')}}" type="text/javascript"> </script>
