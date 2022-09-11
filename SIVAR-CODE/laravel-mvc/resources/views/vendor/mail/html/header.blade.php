b4d243822484c0e2b7b35e297c6d6496a0421aec
William Emmanuel Mazariego Orellana <83888568+Will-Trucker@users.noreply.github.com>
2022-09-11 06:23:59 -0600

    Agregacion de Estilos en el correo pero no sirvexd

---
+++ SIVAR-CODE/laravel-mvc/resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,11 @@
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+@if (trim($slot) === 'SIVAR-TECHSTORE')
+<img src="https://i.postimg.cc/cH5x9kWs/Logo.jpg" class="logo" alt="Logo de SIVAR-TECHSTORE">
+@else
+{{ $slot }}
+@endif
+</a>
+</td>
+</tr>
